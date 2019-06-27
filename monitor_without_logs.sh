e5db955ff4bb4f6859ab7ac4708c6944db4100c9
angelo <angelo.ferrando42@gmail.com>
2019-06-27 09:01:59 +0100

    monitor with no logs added for benchmark generation

--- monitor_without_logs.sh
+++ monitor_without_logs.sh
@@ -5,4 +5,4 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # specify monitor alias path for modules imported by the spec (like deep_subdict)
-swipl -p monitor="$here" "$here"/monitor_bench.pl -- "$@"
+swipl -p monitor="$here" "$here"/monitor_without_logs.pl -- "$@"
