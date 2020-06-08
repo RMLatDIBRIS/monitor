e59072bd00ae785ae50733ab037da1531fdb83d0
davideancona <davide.ancona@unige.it>
2020-06-08 10:29:51 +0200

    monitor  changed arguments order, in accordance with the match predicate renamed variables for readability modified:   deep_subdict.pl

--- deep_subdict.pl
+++ deep_subdict.pl
@@ -4,35 +4,39 @@
 
 % fix by Davide (Jan 22, 2020)
 % deep_subdict(V,_{x:1,y:2}). should succeeds with V = _7026{x:1, y:2} and not V = _7276{}
-deep_subdict(X, D) :- var(X),!,X=D.
 
-% if the first dict is empty we're done
-deep_subdict(_{}, D) :- is_dict(D),!.
+% Davide (Jun 8, 2020)
+% changed arguments order, in accordance with the match predicate
+
+deep_subdict(Event, Pattern) :- var(Pattern),!,Pattern=Event.
+
+% if the pattern is empty we're done
+deep_subdict(Event, _{}) :- is_dict(Event),!.
 
 % dictionaries
-deep_subdict(D1, D2) :-
+deep_subdict(Event,Pattern) :-
 	% ensure they're dicts
-	is_dict(D1),
-	is_dict(D2),
+	is_dict(Pattern),
+	is_dict(Event),
 	!,
-	% take a pair out from the subdict
-	dict_pairs(D1, _, [K-V1|L1]),
-	% find a pair with the same key in the superdict
-	dict_pairs(D2, _, L2),
-	member(K-V2, L2),
+	% take a pair out from the pattern
+	dict_pairs(Pattern, _, [Key-PatternValue|OtherPatternPairs]),
+	% find a pair with the same key in the event
+	dict_pairs(Event, _, EventPairs),
+	member(Key-EventValue, EventPairs),
 	% check wether values are compatible
-	deep_subdict(V1, V2),
+	deep_subdict(EventValue,PatternValue),
 	% drop the already checked pair from the subdict
-	dict_pairs(D1rest, _, L1),
+	dict_pairs(PatternRest, _, OtherPatternPairs),
 	% check the rest of the dict
-	deep_subdict(D1rest, D2).
+	deep_subdict(Event,PatternRest).
 
-% lists
+% lists of events and patterns
 deep_subdict([], []) :- !.
-deep_subdict([X1|L1], [X2|L2]) :-
+deep_subdict([Event|OtherEvents],[Pattern|OtherPatterns]) :-
 	!,
-	deep_subdict(X1, X2),
-	deep_subdict(L1, L2).
+	deep_subdict(Event, Pattern),
+	deep_subdict(OtherEvents, OtherPatterns).
 
 % others
 deep_subdict(X, X).
