9e531df7a76a1a51e7cc35e87d2ce81fb504cf0b
davideancona <davide.ancona@unige.it>
2024-06-06 09:46:30 +0200

    monitor modified:   run-monitor.sh

--- run-monitor.sh
+++ run-monitor.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # get dir of this script
-# https://stackoverflow.com/a/246128/1202636
-# here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-here=/home/davide/git/RMLatDIBRIS/monitor/
+
+here=$(dirname "$0")/
 
 # specify monitor alias path for modules imported by the spec (like deep_subdict)
 monitor="$here"$1".pl"
