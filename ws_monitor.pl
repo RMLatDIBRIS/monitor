f0adce24c304c19a0cb629c927861015feedf50c
davideancona <davide.ancona@unige.it>
2024-06-25 11:51:21 +0200

    monitor deleted:    iot_ws_monitor.pl modified:   ws_monitor.pl new file:   ws_monitor_old.pl renamed:    iot_wss_monitor.pl -> wss_monitor.pl

--- ws_monitor.pl
+++ ws_monitor.pl
@@ -1,5 +1,3 @@
-%% obsolete version, replaced by iot_ws(s)_monitor.pl
-
 :- use_module(library(http/websocket)).
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
@@ -8,68 +6,54 @@
 :- use_module(library(http/json_convert)).
 :- use_module(library(http/http_json)).
 
-:- use_module(trace_expressions_semantics).
+:- use_module(monitor(trace_expressions_semantics)).
 
 :- http_handler(/,http_upgrade_to_websocket(manage_event, []),[]). %%% default options for both the websocket and the http handler 
 
 %% arguments
 %% the server expects a required first argument: the filename containing the specified trace expression
-%% second optional argument: a log file, if not provided no logging is performed
+%% second optional argument: the server port, default is 80
+%% third optional argument: a log file, if not provided no logging is performed
+%% warning: if the log file is provided, then the port is required even for the default value
 
 %% example:
-%% swipl -p node=prolog prolog/server.pl -- http/omitted\ body/204\ response/spec.pl prolog_server_log.txt
-%% -p node=prolog
-%%          required option to indicate the path to func_match.pl (event domain implementation)
-%%  http/omitted\ body/204\ response/spec.pl
-%%          the trace expression (required argument)
-%%  prolog_server_log.txt
-%%          logging enabled to file prolog_server_log.txt (optional argument)
+%% sudo swipl -O -p monitor=pathToPrologMonitor iot_ws_monitor.pl -- RMLspec.pl (port? | (port logFile)?)
 
-% load specification
+% initialization of the state of the worker thread: loads the specification and initializes gobal variable 'state' with it
 
-:- current_prolog_flag(argv, [Spec|_]), use_module(Spec).
+init :- current_prolog_flag(argv, Argv), Argv = [Spec|Rest], use_module(Spec), trace_expression(_, TE), nb_setval(state,TE),
+(Rest = [Port|LogArg] -> atom_number(Port,Addr),nb_setval(port,Addr),(LogArg = [LogFile|_]->open(LogFile,append,Stream),nb_setval(log_file, Stream);nb_setval(log_file,null));nb_setval(port,80),nb_setval(log_file, null)).
 
-server(Port) :- http_server(http_dispatch,[port('10.251.61.71':Port),workers(1)]). %% one worker to guarantee event sequentiality
-%server(Port) :- http_server(http_dispatch,[port('192.168.1.250':Port),workers(1)]). %% one worker to guarantee event sequentiality
+:- thread_initialization(init).
 
-log(Log) :-
-    nb_getval(log,Stream), Stream\==null->  %% optional logging of server activity
-	(Log=(TE,E)->
-	     writeln(Stream,"Trace expression:"),writeln(Stream,TE),writeln(Stream,"Event: "),writeln(Stream,E);
-	 writeln(Stream,"Error")),
+%% predicates to manage the optional log file
+
+:- if(nb_getval(log_file,null)).
+log(_).
+:- else.
+log(Arg) :-
+        nb_getval(log_file,Stream),
+	(Arg=(TE1,E,TE2)->
+	     writeln(Stream,"Initial state:"),writeln(Stream,TE1),writeln(Stream,"Event: "),writeln(Stream,E),writeln(Stream,"Final state:"),writeln(Stream,TE2);
+	 writeln(Stream,"Logging error")),
 	nl(Stream),
-	flush_output(Stream);
-    true.
+	flush_output(Stream).
+:- endif.
+
+server(Port) :- http_server(http_dispatch,[port(Port),workers(10)]). 
 		 
 manage_event(WebSocket) :-
-    ws_receive(WebSocket, Msg, [format(json),value_string_as(atom)]), %% value_string_as(atom) passed as option to json_read_dict/3
+    ws_receive(WebSocket, Msg, [format(json)]), 
     (Msg.opcode==close ->
 	     true;
 	 E=Msg.data,
 	       nb_getval(state,TE1),
-	       log((TE1,E)),
-	       (next(TE1,E,TE2) -> nb_setval(state,TE2),Reply=E; Reply=_{error:true}),
-	       %% (next(TE1,E,TE2) -> nb_setval(state,TE2),Reply='{"error":false}'; Reply='{"error":true}',log(error)),
-	       %% next line: more detailed information computed in case an error occurs
-	       %% (next(TE1,Msg.data,TE2) -> nb_setval(state,TE2),Reply='{"error":false}'; term_string(TE1,State),atomics_to_string(['{"error":true, "state":',State,', "event":', Msg.data, '}'], Reply)),
+	       (next(TE1,E,TE2) -> nb_setval(state,TE2),log((TE1,E,TE2)),Reply=_{error:false,data:E}; log((TE1,E,error)),Reply=_{error:true,data:E}),
 	       atom_json_dict(Json,Reply,[as(string)]),
 	       ws_send(WebSocket,string(Json)),
 	       manage_event(WebSocket)).
 
-%% old patched version to avoid problems with json dicts, resolved by using value_string_as(atom) option
-%% manage_event(WebSocket) :-
-%%     ws_receive(WebSocket, Msg), %% uses string as default format, format(json) does not work properly, dict implementation not stable 
-%%     (Msg.opcode==close ->
-%%      true;
-%%      nb_getval(state,TE1),
-%%      log((TE1,Msg.data)),
-%%      (next(TE1,Msg.data,TE2) -> nb_setval(state,TE2),Reply='{"error":false}'; Reply='{"error":true}'),
-%%      %% next line: more detailed information computed in case an error occurs
-%%      %% (next(TE1,Msg.data,TE2) -> nb_setval(state,TE2),Reply='{"error":false}'; term_string(TE1,State),atomics_to_string(['{"error":true, "state":',State,', "event":', Msg.data, '}'], Reply)),
-%%      ws_send(WebSocket,text(Reply)),
-%%      manage_event(WebSocket)).
-		
-exception(undefined_global_variable, state, retry) :- trace_expression(_, TE), nb_setval(state,TE).
-exception(undefined_global_variable, log, retry) :- (current_prolog_flag(argv, [_,LogFile|_])->open(LogFile,append,Stream);Stream=null),nb_setval(log, Stream).
+%% starts the server
 
-:- server(80).
+:- nb_getval(port,Port),initialization(server(Port)).
+
